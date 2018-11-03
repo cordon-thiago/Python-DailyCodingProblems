a5d183131f53502bc45d5c1b39eafd630103a595
thiago <thiaguscr@gmail.com>
2018-11-03 14:43:49 -0300

    Add Problem comment

--- Problem1.py
+++ Problem1.py
@@ -1,4 +1,11 @@
 #Desafio 1 Daily coding problems
+
+'''
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+Bonus: Can you do this in one pass?
+'''
+
 numList = [10,15,3,5]
 k = 20
 
