0ce9e465a57faafeed7afbaba21a6e549ae3a398
cordon-thiago <thiaguscr@gmail.com>
2020-02-24 20:36:13 +0000

    Comment added to Problem2

--- Problem2.py
+++ Problem2.py
@@ -1,4 +1,6 @@
 # Problem 2 - Daily Coding Problem
+# Teste SourceTree
+
 '''
 Given an array of integers, return a new array such that each element at index i of the new array is the product of all the
 numbers in the original array except the one at i.
